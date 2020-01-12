ab0561e56d1bb14dfd878d129d2f40c600ee9705
Lucas Kane <serhii-kostiuchok@ukr.net>
2020-01-13 01:39:56 +0200

    13-01-2020-0139

--- Problem-1/main.c
+++ Problem-1/main.c
@@ -1,35 +1,18 @@
-// gcc -Wall -g -D DBG_E_OLYMP main.c -o a.out
-// #define DBG_E_OLYMP
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MIN_LIMIT 10
-#define MAX_LIMIT 99
-
 int main()
 {
-    unsigned int num = 0u;
-
-#ifndef DBG_E_OLYMP
-
+#ifndef DEBUG
     freopen("input", "r", stdin);
     freopen("output", "w", stdout);
-
 #endif
 
-    scanf("%u", &num);
+    int n;
+    scanf("%d", &n);
 
-    if (MIN_LIMIT <= num && num <= MAX_LIMIT)
-        printf("%u %u\n", num / 10, num % 10);
-
-
-#ifndef DBG_E_OLYMP
-
-    fclose(stdin);
-    fclose(stdout);
-
-#endif
+    if (10 <= n && n <= 99)
+        printf("%d %d\n", n / 10, n % 10);
 
     return EXIT_SUCCESS;
 }
