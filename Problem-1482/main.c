da98d66ea8b3abc039451611a1034c7706910385
Сергій Костючок <serhii-kostiuchok@ukr.net>
2020-01-14 02:33:01 +0200

    14-01-202-0232

--- Problem-1482/main.c
+++ Problem-1482/main.c
@@ -1,4 +1,48 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Point;
+struct Point *createPoint(double, double);
+double getDistance(struct Point *, struct Point *);
+
+struct Point
+{
+    double x;
+    double y;
+};
+
+struct Point *createPoint(double x, double y)
+{
+    struct Point *point = malloc(sizeof(struct Point));
+
+    point->x = x;
+    point->y = y;
+
+    return point;
+}
+
+double getDistance(struct Point *p1, struct Point *p2)
+{
+    double dx = p1->x - p2->x;
+    double dy = p1->y - p2->y;
+
+    return sqrt(dx * dx + dy * dy);
+}
+
 int main()
 {
+    struct Point *p1 = createPoint(3, 4);
+    struct Point *p2 = createPoint(1, 2);
+
+    p1->x = 1;
+    p1->y = 1;
+    p2->x = 2;
+    p2->y = 2;
+
+    double distance = getDistance(p1, p2);
+
+    printf("%f\n", distance);
+
     return 0;
 }
---
+++ Problem-1482/main.s
@@ -0,0 +1,63 @@
+	.file	"main.c"
+	.text
+	.globl	f
+	.type	f, @function
+f:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	movl	8(%ebp), %eax
+	cmpl	%eax, 12(%ebp)
+	cmovle	12(%ebp), %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	f, .-f
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	pushl	$13
+	pushl	$12
+	call	f
+	addl	$8, %esp
+	movl	%eax, -4(%ebp)
+	movl	-4(%ebp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB2:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE2:
+	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
+	.section	.note.GNU-stack,"",@progbits
