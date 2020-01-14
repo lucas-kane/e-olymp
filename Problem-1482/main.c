591e476dee7f9de15b183512e0751bd6d15bb133
Сергій Костючок <serhii-kostiuchok@ukr.net>
2020-01-14 03:52:05 +0200

    14-01-2020-0352

--- Problem-1482/main.c
+++ Problem-1482/main.c
@@ -1,10 +1,16 @@
+#include <malloc.h>
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
+/*
+*   Declaration...
+*/
 struct Point;
 struct Point *createPoint(double, double);
+void freePoint(struct Point *);
 double getDistance(struct Point *, struct Point *);
+double getX(struct Point *);
+double getY(struct Point *);
 
 struct Point
 {
@@ -14,12 +20,21 @@ struct Point
 
 struct Point *createPoint(double x, double y)
 {
-    struct Point *point = malloc(sizeof(struct Point));
+    struct Point *point = (struct Point *)malloc(sizeof(struct Point));
+
+    if (!point)
+        return NULL;
 
     point->x = x;
     point->y = y;
 
     return point;
+}
+
+void freePoint(struct Point *p)
+{
+    if (p)
+        free(p);
 }
 
 double getDistance(struct Point *p1, struct Point *p2)
@@ -30,19 +45,23 @@ double getDistance(struct Point *p1, struct Point *p2)
     return sqrt(dx * dx + dy * dy);
 }
 
+double getX(struct Point *p) { return p->x; }
+double getY(struct Point *p) { return p->y; }
+
 int main()
 {
     struct Point *p1 = createPoint(3, 4);
     struct Point *p2 = createPoint(1, 2);
 
-    p1->x = 1;
-    p1->y = 1;
-    p2->x = 2;
-    p2->y = 2;
-
     double distance = getDistance(p1, p2);
 
-    printf("%f\n", distance);
+    printf("Point P1(%3.1f, %3.1f).\n", getX(p1), getY(p1));
+    printf("Point P2(%3.1f, %3.1f).\n", getX(p2), getY(p2));
+
+    freePoint(p1);
+    freePoint(p2);
+
+    printf("Distance: %3.4f\n", distance);
 
     return 0;
 }
--- Problem-1482/main.s
+++ Problem-1482/main.s
@@ -1,63 +1,233 @@
 	.file	"main.c"
 	.text
-	.globl	f
-	.type	f, @function
-f:
+	.globl	createPoint
+	.type	createPoint, @function
+createPoint:
 .LFB0:
 	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	movl	8(%ebp), %eax
-	cmpl	%eax, 12(%ebp)
-	cmovle	12(%ebp), %eax
-	popl	%ebp
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movsd	%xmm0, -24(%rbp)
+	movsd	%xmm1, -32(%rbp)
+	movl	$16, %edi
+	call	malloc@PLT
+	movq	%rax, -8(%rbp)
+	cmpq	$0, -8(%rbp)
+	jne	.L2
+	movl	$0, %eax
+	jmp	.L3
+.L2:
+	movq	-8(%rbp), %rax
+	movsd	-24(%rbp), %xmm0
+	movsd	%xmm0, (%rax)
+	movq	-8(%rbp), %rax
+	movsd	-32(%rbp), %xmm0
+	movsd	%xmm0, 8(%rax)
+	movq	-8(%rbp), %rax
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
-	.size	f, .-f
-	.globl	main
-	.type	main, @function
-main:
+	.size	createPoint, .-createPoint
+	.globl	freePoint
+	.type	freePoint, @function
+freePoint:
 .LFB1:
 	.cfi_startproc
-	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
-	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
-	subl	$16, %esp
-	call	__x86.get_pc_thunk.ax
-	addl	$_GLOBAL_OFFSET_TABLE_, %eax
-	pushl	$13
-	pushl	$12
-	call	f
-	addl	$8, %esp
-	movl	%eax, -4(%ebp)
-	movl	-4(%ebp), %eax
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	cmpq	$0, -8(%rbp)
+	je	.L6
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+.L6:
+	nop
 	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE1:
-	.size	main, .-main
-	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
-	.globl	__x86.get_pc_thunk.ax
-	.hidden	__x86.get_pc_thunk.ax
-	.type	__x86.get_pc_thunk.ax, @function
-__x86.get_pc_thunk.ax:
+	.size	freePoint, .-freePoint
+	.globl	getDistance
+	.type	getDistance, @function
+getDistance:
 .LFB2:
 	.cfi_startproc
-	movl	(%esp), %eax
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	-24(%rbp), %rax
+	movsd	(%rax), %xmm0
+	movq	-32(%rbp), %rax
+	movsd	(%rax), %xmm1
+	subsd	%xmm1, %xmm0
+	movsd	%xmm0, -8(%rbp)
+	movq	-24(%rbp), %rax
+	movsd	8(%rax), %xmm0
+	movq	-32(%rbp), %rax
+	movsd	8(%rax), %xmm1
+	subsd	%xmm1, %xmm0
+	movsd	%xmm0, -16(%rbp)
+	movsd	-8(%rbp), %xmm0
+	movapd	%xmm0, %xmm1
+	mulsd	-8(%rbp), %xmm1
+	movsd	-16(%rbp), %xmm0
+	mulsd	-16(%rbp), %xmm0
+	addsd	%xmm1, %xmm0
+	call	sqrt@PLT
+	movq	%xmm0, %rax
+	movq	%rax, -40(%rbp)
+	movsd	-40(%rbp), %xmm0
+	leave
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE2:
+	.size	getDistance, .-getDistance
+	.globl	getX
+	.type	getX, @function
+getX:
+.LFB3:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movsd	(%rax), %xmm0
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	getX, .-getX
+	.globl	getY
+	.type	getY, @function
+getY:
+.LFB4:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movsd	8(%rax), %xmm0
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE4:
+	.size	getY, .-getY
+	.section	.rodata
+.LC4:
+	.string	"Point P1(%3.1f, %3.1f).\n"
+.LC5:
+	.string	"Point P2(%3.1f, %3.1f).\n"
+.LC6:
+	.string	"Distance: %3.4f\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movsd	.LC0(%rip), %xmm1
+	movsd	.LC1(%rip), %xmm0
+	call	createPoint
+	movq	%rax, -8(%rbp)
+	movsd	.LC2(%rip), %xmm1
+	movsd	.LC3(%rip), %xmm0
+	call	createPoint
+	movq	%rax, -16(%rbp)
+	movq	-16(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	getDistance
+	movq	%xmm0, %rax
+	movq	%rax, -24(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	getY
+	movsd	%xmm0, -40(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	getX
+	movsd	-40(%rbp), %xmm1
+	leaq	.LC4(%rip), %rdi
+	movl	$2, %eax
+	call	printf@PLT
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	getY
+	movsd	%xmm0, -40(%rbp)
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	getX
+	movsd	-40(%rbp), %xmm1
+	leaq	.LC5(%rip), %rdi
+	movl	$2, %eax
+	call	printf@PLT
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	freePoint
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	freePoint
+	movsd	-24(%rbp), %xmm0
+	leaq	.LC6(%rip), %rdi
+	movl	$1, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	main, .-main
+	.section	.rodata
+	.align 8
+.LC0:
+	.long	0
+	.long	1074790400
+	.align 8
+.LC1:
+	.long	0
+	.long	1074266112
+	.align 8
+.LC2:
+	.long	0
+	.long	1073741824
+	.align 8
+.LC3:
+	.long	0
+	.long	1072693248
 	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
 	.section	.note.GNU-stack,"",@progbits
