8223b4d2c1bcd517eeb6e380251e68bf145af1b5
Сергій Костючок <serhii-kostiuchok@ukr.net>
2020-01-17 22:45:09 +0200

    17-01-2020-2245

--- Problem-1482/main.c
+++ Problem-1482/main.c
@@ -1,30 +1,4 @@
-#include <assert.h>
-#include <stdio.h>
-
-void swap(int *, int *);
-
 int main()
 {
-    int *pa, a = 2;
-    int *pb, b = 3;
-
-    pa = &a;
-    pb = &b;
-
-    printf("%d %d\n", *pa, *pb);
-    swap(pa, pb);
-    printf("%d %d\n", *pa, *pb);
-
-    printf("%p %p\n", &pa, &a);
-
     return 0;
-}
-
-void swap(int *a, int *b)
-{
-    assert(a && b);
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
